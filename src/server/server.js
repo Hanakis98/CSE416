fef692e70031d3dec34a942225889f1d44c26452
KhiemPhi <khiemais@gmail.com>
2021-03-14 21:08:49 -0400

    database connect success, fixed by changing mongoose version, need re-run npm build when pull

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,19 @@
-//Connection to Mongo
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 
-// Connection URL
-const uri = 'mongodb+srv://ted:$[rfYMsIXkluwYrYaA]@cse416.23kfi.mongodb.net/CSE416?retryWrites=true';
+const test = require('assert');
+
+// Connection url
+
+const uri = 'mongodb+srv://ted:<rfYMsIXkluwYrYaA>@cse416.23kfi.mongodb.net/CSE416?retryWrites=true&w=majority';
+
+// Database Name
 const dbName = "CSE416"
 
 const mongoose = require("mongoose")
 
-mongoose.connect(uri, {  useNewUrlParser: true })
-  .then(() =>  console.log('connection successful'))
-  .catch((err) => console.error(err));
-
-
-  var db = mongoose.connection; 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db(dbName).listCollections()
+  console.log(collection)
+  client.close();
+});
