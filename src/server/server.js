6987659ab69ef49f960b798218b6d859b5c0a696
Teddy Hanakis <teddyhanakis@teddys-mbp.myfiosgateway.com>
2021-03-23 18:15:42 -0400

    pre fix

--- src/server/server.js
+++ src/server/server.js
@@ -1,30 +1,29 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
 
 const server = express();
 
 const body_parser = require("body-parser");
+
 server.use(body_parser.json());
+
 const port = 4000;
-const test = require('assert');
-// Connection url
-const uri = 'mongodb+srv://ted:<rfYMsIXkluwYrYaA>@cse416.23kfi.mongodb.net/CSE416?retryWrites=true&w=majority';
+
+const db = require('./db')
+
 // Database Name
-const collectionName = "CSE416"
 const dbName = "CSE416"
-
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, keepAlive: 1 });
+const collectionName = "Users"
 
 server.listen(port, () => {
     console.log(`Server listening at ${port}`);
 });
 
-client.connect(err => {
-   const collection = client.db(dbName).collection(collectionName);
-    // perform actions on the collection object
-    collection.find().toArray();
-    console.log('connected');
+db.initialize(dbName, collectionName, function(collection) { // successCallback
+    collection.find().toArray(function(err, result){
+        if (err) throw err;
+            console.log(result);
+    })
 
-
+}, function(err) { // failureCallback
+    throw (err);
 });
