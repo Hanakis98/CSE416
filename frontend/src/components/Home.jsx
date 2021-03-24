93201ff783c751a4117ccbef0114d3924a9df392
KhiemPhi <khiemais@gmail.com>
2021-03-24 09:52:48 -0400

    fix heroku hosting

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Container, Row, Col, Jumbotron, Button } from 'reactstrap';
 
+
+
+
 export default function Home() {
   return (
     <Container>
