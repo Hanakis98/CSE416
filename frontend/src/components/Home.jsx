d7cb92bfb68def9a8851bc9e7677ca1eb34af84b
cchsuper <cchsuper@gmail.com>
2021-03-24 12:08:10 -0400

    Added login elements on Home screen

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Container, Row, Col, Jumbotron, Button } from 'reactstrap';
-
-
-
+import { Container, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 export default function Home() {
   return (
-    <Container>
-      <p>Login</p>
-      <Button>Submit</Button>
+    <Container style={{display: 'flex', alignItems: 'center', justifyContent: 'center',}}>
+      <Form>
+        <FormGroup>
+          <Label for="login_id">ID</Label>
+          <Input type="text" name="id" id="login_id" placeholder="110047354" style={{width:200}} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="login_password">Password</Label>
+          <Input type="password" name="password" id="login_password" placeholder="" style={{width:200}} />
+      </FormGroup>
+        <Button>Submit</Button>
+      </Form>
     </Container>
   );
 }
