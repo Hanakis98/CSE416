1033c8df7eaf35c1bd9983677fcae6b0bc437edd
cchsuper <cchsuper@gmail.com>
2021-03-24 00:00:04 -0400

    Added react-router pages and reactstrap navbar

---
+++ frontend/src/components/Degrees.jsx
@@ -0,0 +1,16 @@
+import { React, Component } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Container, Row, Col, Jumbotron, Button } from 'reactstrap';
+
+export default class Degrees extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        };
+    }
+    render(){
+        return (
+            <p>ok</p>
+        );
+    }
+}
