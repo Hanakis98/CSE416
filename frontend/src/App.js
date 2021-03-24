fdf1310b61c18bb5eb8912da2c5166d2e2ae20b1
KhiemPhi <khiemais@gmail.com>
2021-03-24 11:19:33 -0400

    new react heroku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,4 @@
 
-<<<<<<< HEAD
-function sayHello(params) {
-
-    fetch("http://localhost:3001/students/allStudents").then(response =>
-    response.json().then(data => ({
-      data: data,
-      status: response.status
-    })
-    ).then(res => {
-      console.log(res.status, res.data)
-    }));
-
-    fetch("http://localhost:3001/advisors/allGPDs").then(response =>
-    response.json().then(data => ({
-      data: data,
-      status: response.status
-    })
-    ).then(res => {
-      console.log(res.status, res.data)
-    }));
-=======
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Navigation, Nav2, Footer, Home, About, Contact, Reactstrap, StudentHome, Students, Degrees, Courses, Trends } from "./components";
@@ -42,7 +21,6 @@ export default function App() {
       </Router>
     </div>
   );
->>>>>>> 93201ff783c751a4117ccbef0114d3924a9df392
 
     fetch("http://localhost:3001/courses/allCourses").then(response =>
     response.json().then(data => ({
